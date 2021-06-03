737e873b0da1bd7c44af0705e77f2f8f5d8a36d2
Annas <61279556+annasajkh@users.noreply.github.com>
2021-06-03 22:42:58 +0700

    Update Game.java

--- core/src/com/github/annasajkh/Game.java
+++ core/src/com/github/annasajkh/Game.java
@@ -34,37 +34,36 @@ public class Game extends ApplicationAdapter
 	boolean remove = false;
 	
 	public static Color hsvToRgba(float hue, float saturation, float value, float alpha)
-    {
+    	{
+		int h = (int) (hue * 6);
+		float f = hue * 6 - h;
+		float p = value * (1 - saturation);
+		float q = value * (1 - f * saturation);
+		float t = value * (1 - (1 - f) * saturation);
 
-        int h = (int) (hue * 6);
-        float f = hue * 6 - h;
-        float p = value * (1 - saturation);
-        float q = value * (1 - f * saturation);
-        float t = value * (1 - (1 - f) * saturation);
-
-        switch (h)
-        {
-            case 0:
-                return new Color(value, t, p, alpha);
-            case 1:
-                return new Color(q, value, p, alpha);
-            case 2:
-                return new Color(p, value, t, alpha);
-            case 3:
-                return new Color(p, q, value, alpha);
-            case 4:
-                return new Color(t, p, value, alpha);
-            case 5:
-                return new Color(value, p, q, alpha);
-            default:
-                throw new RuntimeException("Something went wrong when converting from HSV to RGB. Input was " +
-                                           hue +
-                                           ", " +
-                                           saturation +
-                                           ", " +
-                                           value);
-        }
-    }
+		switch (h)
+		{
+		    case 0:
+			return new Color(value, t, p, alpha);
+		    case 1:
+			return new Color(q, value, p, alpha);
+		    case 2:
+			return new Color(p, value, t, alpha);
+		    case 3:
+			return new Color(p, q, value, alpha);
+		    case 4:
+			return new Color(t, p, value, alpha);
+		    case 5:
+			return new Color(value, p, q, alpha);
+		    default:
+			throw new RuntimeException("Something went wrong when converting from HSV to RGB. Input was " +
+						   hue +
+						   ", " +
+						   saturation +
+						   ", " +
+						   value);
+		}
+	}
 	
 	
 	@Override
